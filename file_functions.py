b2547c100d079f3e7fd38e2ece0de6b946a08faf
kicillof <cuentasdebusso@hotmail.com>
2022-11-05 20:17:12 -0300

    Sent file functions (read, save) to another .py
    - Started the process of separating some functions and putting them into their own .py file. The idea is to end up with a much tidier notebook to execute.
    - Merged a couple cells.

---
+++ file_functions.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+url_list = []
+
+def save_list(vehicle_type, url_list):
+    jsonString = json.dumps(url_list)
+    # path = "./dataset/" + vehicle_type + "/img_urls.json"
+    path = os.path.join('./dataset',vehicle_type,'img_urls.json')
+    jsonFile = open(path, "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+
+def read_list(vehicle_type: str):
+    # path = "./dataset/" + vehicle_type + "/img_urls.json"
+    path = os.path.join('./dataset',vehicle_type,'img_urls.json')
+    fileObject = open(path, "r")
+    jsonContent = fileObject.read()
+    try:
+        url_list = json.loads(jsonContent)
+    except:
+        #json is empty
+        url_list = []
+        pass
+    return url_list
